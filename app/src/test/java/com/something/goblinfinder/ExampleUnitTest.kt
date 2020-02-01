1d6949ece8d1dc6b72e53397092b40740d03c6af
callags <s.callaghan513@gmail.com>
2020-02-01 14:52:26 -0500

    Final publish 2/1/20

---
+++ app/src/test/java/com/something/goblinfinder/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.something.goblinfinder
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * something local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class somethingUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
